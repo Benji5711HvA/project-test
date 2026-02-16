11faa911116f95e70e9a1ad1601a238902ec0783
Benji5711HvA <benjamin.sadri.milani@hva.nl>
2026-02-16 15:11:23 +0100

    db werkt

--- server.js
+++ server.js
@@ -1,87 +1,94 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
- 
-const uri = process.env.MONGODB_URI;
+const { MongoClient, ObjectId } = require("mongodb");
 
-const users = [
-    {
-        username: "Benji5711",
-        password: "asdfghjkl"
-    }
-];
+const uri = process.env.URI;
+const client = new MongoClient(uri);
 
-app.use(express.static('static'));
+let usersCollection;
 
-
-
+// Middleware
+app.use(express.static("static"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-app.listen(3000, () => {
-    console.log("server is running on http://localhost:3000")
-})
+// Routes
+app.get("/", home);
+app.get("/detail", toonDetail);
 
-app.get("/", home)
-app.get("/detail", toonDetail)
+app.get("/register", showRegister);
+app.post("/register", handleRegister);
 
-app.get("/register", showRegister)
-app.post("/register", handleRegister)
+app.get("/login", showLogin);
+app.post("/login", handleLogin);
 
-app.get("/login", showLogin)
-app.post("/login", handleLogin)
+app.get("/dashboard", dashboard);
 
-app.get("/dashboard", dashboard)
+// DB connect + start server
+async function startServer() {
+  try {
+    await client.connect();
+    console.log("✅ Verbonden met MongoDB");
 
+    const db = client.db("project-test"); // <-- pas aan als jouw db anders heet
+    usersCollection = db.collection("users");
 
+    app.listen(3000, () => {
+      console.log("server is running on http://localhost:3000");
+    });
+  } catch (error) {
+    console.error("❌ Database connectie mislukt:", error);
+  }
+}
 
+startServer();
+
+// Controllers
 function home(req, res) {
-    res.send('Hello world')
+  res.send("Hello world");
 }
 
 function toonDetail(req, res) {
-    let movie = {
-        title: "Shawshank!!!!!!!!",
-        description: "Dit is een film. Hallo",
-    };
+  let movie = {
+    title: "Shawshank!!!!!!!!",
+    description: "Dit is een film. Hallo",
+  };
 
-    res.render("detail", { data: movie });
+  res.render("detail", { data: movie });
 }
-
 
 function showRegister(req, res) {
   res.render("pages/register");
 }
 
+async function handleRegister(req, res) {
+  const { username, password } = req.body;
 
-function handleRegister(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
+  // Check of gebruiker al bestaat
+  const existingUser = await usersCollection.findOne({ username });
+  if (existingUser) {
+    return res.send("Gebruikersnaam bestaat al");
+  }
 
-  users.push({
-    username: username,
-    password: password
-  });
-
-  console.log(users);
+  // Opslaan in MongoDB
+  await usersCollection.insertOne({ username, password });
 
   res.redirect("/login");
 }
-
 
 function showLogin(req, res) {
   res.render("pages/login");
 }
 
+async function handleLogin(req, res) {
+  const { username, password } = req.body;
 
-function handleLogin(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  const user = users.find(function(u) {
-    return u.username === username;
-  });
+  const user = await usersCollection.findOne({ username });
 
   if (user && user.password === password) {
     res.redirect("/dashboard");
@@ -89,7 +96,6 @@ function handleLogin(req, res) {
     res.send("Login mislukt");
   }
 }
-
 
 function dashboard(req, res) {
   res.send("Welkom op je dashboard");
